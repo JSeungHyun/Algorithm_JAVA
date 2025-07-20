ece1f11019e0b37ae934cb8f66acae08c761b834
JSeungHyun <83762088+JSeungHyun@users.noreply.github.com>
2025-07-20 22:23:34 +0900

    [Silver V] Title: 단어 정렬, Time: 460 ms, Memory: 23156 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,26 +1,21 @@
 import java.util.*;
 import java.io.*;
-import java.util.stream.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Set<String> stringSet = new HashSet<>();
         int N = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
 
         for (int i = 0; i < N; i++) {
-            stringSet.add(br.readLine());
+            set.add(br.readLine());
         }
 
-        List<String> list = stringSet.stream()
+        set.stream()
                 .sorted((a, b) -> {
                     if (a.length() != b.length()) return a.length() - b.length();
-                    return a.compareTo(b);
+                    else return a.compareTo(b);
                 })
-                .collect(Collectors.toList());
-
-        for (String s : list) {
-            System.out.println(s);
-        }
+                .forEach(item -> System.out.println(item));
     }
 }
