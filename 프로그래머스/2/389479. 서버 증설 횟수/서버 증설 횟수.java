9d730d2643bfd4c91112372fb42d5d5b5768b009
JSeungHyun <83762088+JSeungHyun@users.noreply.github.com>
2026-03-03 22:40:16 +0900

    [level 2] Title: 서버 증설 횟수, Time: 4.07 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -5,17 +5,21 @@ class Solution {
         int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        for (int i=0; i<players.length; i++) {
-            boolean flag = false;
-            while (!pq.isEmpty() && pq.peek() <= i) pq.poll();
-            int player = players[i];
-            int need = player / m; // 필요 서버의 수
-            int cur = pq.size();
-            
-            if (need > cur) {
-                answer += need - cur;
-                for (int j=0; j < need - cur; j++) pq.add(i + k);
+        for (int i=0; i< players.length; i++) {
+            // 시간 만료된 서버 제거
+            while (!pq.isEmpty() && pq.peek() <= i) {
+                pq.poll();
             }
+            
+            int player = players[i];
+            if (player == 0) continue;
+            
+            int needServer = (player / m) - pq.size();
+            if (needServer <= 0) continue;
+            
+            answer += needServer;
+            for (int j=0; j<needServer; j++) pq.add(i + k);
+                
         }
         
         return answer;
